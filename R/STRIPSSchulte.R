2392db9657918048b7f623dc4cf849d61cb2af23
Jarad Niemi <niemi@iastate.edu>
2016-10-20 14:18:09 -0500

    Add package documentation

---
+++ R/STRIPSSchulte.R
@@ -0,0 +1,19 @@
+#' STRIPSSchulte: A package for avian counts from the lab of Lisa Schulte-Moore.
+#'
+#' This package contains counts on 16 relatively abundant species as well as
+#' community abundance, richness, and diversity measurements.
+#'
+#' @section STRIPSSchulte data sets:
+#' \code{\link{avian_abbrev}}
+#' \code{\link{avian_common_count}}
+#' \code{\link{avian_community}}
+#' \code{\link{avian_groups}}
+#' \code{\link{avian_surveys}}
+#'
+#' @seealso \code{\link{avian_abbrev}}, \code{\link{avian_common_count}},
+#' \code{\link{avian_community}}, \code{\link{avian_groups}},
+#' \code{\link{avian_surveys}}
+#'
+#' @docType package
+#' @name STRIPSSchulte
+NULL
