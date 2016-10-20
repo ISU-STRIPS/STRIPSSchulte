2fb46f292b9b100c35cbbc4e9592cc0972e8a36a
Jarad Niemi <niemi@iastate.edu>
2016-10-20 13:44:56 -0500

    Remove unnecessary columns from avian community data

--- data-raw/avian_community.R
+++ data-raw/avian_community.R
@@ -2,8 +2,7 @@ library(dplyr)
 
 avian_community <- readr::read_csv("STRIPsBirdsAllObs.csv") %>%
   setNames(tolower(names(.))) %>%
-  left_join(readr::read_csv("site_to_watershed.csv")) %>%
-  select(survey, year, julian, watershed, tba, richness, simp.ind) %>%
+  select(survey, tba, richness, simp.ind) %>%
   rename(abundance = tba,
          diversity = simp.ind)
 
