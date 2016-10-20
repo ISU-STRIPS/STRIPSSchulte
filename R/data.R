1cf7f825dd675e4711d38afc640f695dfbf1f321
Jarad Niemi <niemi@iastate.edu>
2016-10-20 11:41:51 -0500

    Add avian_groups data

--- R/data.R
+++ R/data.R
@@ -6,3 +6,12 @@
 #' @format A data.frame with two variables: \code{abbrev} and
 #' \code{common_name}
 "avian_abbrev"
+
+#' Avian groups
+#'
+#' Whether a species is a grassland species and whether the species is of
+#' greatest conservation need.
+#'
+#' @format A data.frame with three variables: \code{abbrev},
+#' \code{grassland_species}, and \code{species_of_greatest_conservation_need}
+"avian_groups"
